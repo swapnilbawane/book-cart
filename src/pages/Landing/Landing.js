5a1175600fe7b6f58b391c595a87b6af1a617c27
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-05-27 15:02:25 +0530

    landing loading done complete

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -5,6 +5,7 @@ import { useData } from "../../context/data-context";
 import { LandingCategories } from "./LandingCategories";
 import { useAuth } from "../../context/auth-context";
 import { Link } from "react-router-dom";
+import { LoadingLanding } from "./LoadingLanding";
 
 export function Landing() { 
 
@@ -22,57 +23,69 @@ return (
 <h1> Landing app. </h1>
      
 <div className="landing-comp"> 
-
-<div className="landing-head"> 
+{ apiData?.category?.categories
+?
+(
+    <>
+    <div className="landing-head"> 
     
-{
-apiData?.category?.categories 
-?    
-<div>
-{ 
-apiData?.category?.categories?.map((item) => { return (
-<div key={item._id}>    
-<LandingCategories {...item} />
-<Link to={"/category/"+item._id}> Explore </Link>
-</div> 
-);
-
-})
-
-}
-</div>
-: <h1> Loading categories.. Please wait.. </h1>
-}
-
+    {
+        
+    <>
+    { 
+    apiData?.category?.categories?.map((item) => { return (
+    <div key={item._id}>    
+    <LandingCategories {...item} />
+    <Link to={"/category/"+item._id}> Explore </Link>
+    </div> 
+    );
     
-</div>
-
-<div className="landing-sample-div"> 
-</div>
+    })
     
-<div className="landing-collection">
-
-    <div className="landing-collection-item"> 
-        <div className="blank-space"> </div>
-        <div> 
-        <p> NEW ARRIVALS </p> 
-        <h3> Best of Yuval Harari </h3>  
-        <p> Check out critically acclaimed books from bestselling author </p>
-        </div>
-        </div>
+    }
+    </>
     
-    <div className="landing-collection-item"> 
+    }
+    
+        
+    </div>
+    
+    <div className="landing-sample-div"> 
+    </div>
+        
+    <div className="landing-collection">
+    
+        <div className="landing-collection-item"> 
             <div className="blank-space"> </div>
             <div> 
             <p> NEW ARRIVALS </p> 
-            <h3> Ruskin Bond's Full Collection </h3>  
-            <p> Cosy yourself with warmth of stories by Ruskin Bond. Entire set now available. </p>
+            <h3> Best of Yuval Harari </h3>  
+            <p> Check out critically acclaimed books from bestselling author </p>
             </div>
             </div>
-      
-</div>
+        
+        <div className="landing-collection-item"> 
+                <div className="blank-space"> </div>
+                <div> 
+                <p> NEW ARRIVALS </p> 
+                <h3> Ruskin Bond's Full Collection </h3>  
+                <p> Cosy yourself with warmth of stories by Ruskin Bond. Entire set now available. </p>
+                </div>
+                </div>
+          
+    </div>
+    </>
+)
+
+: <LoadingLanding />
+}
    
+
+
+
 </div>
+
+{/* sfsfsf */}
 
 </> 
 ); 
---
+++ src/pages/Landing/LoadingLanding.js
@@ -0,0 +1,8 @@
+export function LoadingLanding() { 
+    return(
+        <>
+        <h1> Loading products.. please wait.. </h1>
+        <p> Excited to share: We are now official partners of the La Memoribilia Club!  </p>
+        </>
+    ); 
+}
