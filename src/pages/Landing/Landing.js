4100a8d49dc12e08cd6775e5f5112acd95b4a107
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-05-28 21:23:41 +0530

    refactor Landing.js

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -6,18 +6,12 @@ import { LoadingLanding } from "./LoadingLanding";
 import { LandingComponent } from "./LandingComponent";
 import { useErrorContextApp } from "../../context-APIErrorManagement/error-context";
 
-import { LandingCategories } from "./LandingCategories";
-import { Link } from "react-router-dom";
-
-
 export function Landing() { 
 
     const {isLoggedIn} = useAuth();
     const {apiData} = useData(); 
     const { error } = useErrorContextApp(); 
-    // console.log("apiData category length",apiData.category.length);
-    // console.log("apiData categories length",apiData.category.categories.length);
-   console.log("error from landing page", error);
+ 
 
 return (
 <> 
