443547eb2f724e8475a5925ef66e5237b4dab44d
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-06-06 19:19:17 +0530

    address get implement

--- src/pages/User/Address.jsx
+++ src/pages/User/Address.jsx
@@ -1,10 +1,13 @@
 import { useAuth } from "../../context/auth-context";
+import { useAddress } from "../../context/address-context";
 import { ProfileHeader } from "./ProfileHeader";
 import "./User.css"; 
 
 export function Address() { 
 
     const { userDetails} = useAuth(); 
+    const { addresses } = useAddress(); 
+    console.log("address",addresses);
 
     return(
         <> 
@@ -12,7 +15,24 @@ export function Address() {
 
         <div className="user-details"> 
         <p> <b className="strong"> Address </b>  </p>
-      
+        { 
+        addresses.map((item,index)=> { 
+            return (
+                <div key={index} className="address-single">
+                <p> { item.buildingName }, {item.flatNumber}  </p>
+                
+                <p> {item.locality},  {item.area} </p>
+             
+                <p> {item.city} </p>
+                <p> Pincode: {item.pinCode} </p>
+                </div>
+            ); 
+        })
+        }
+
+        <button className="button-address"> Add Address </button> 
+        <button className="button-deleteadd"> Delete Address </button>
+        
         </div>
         </>
     ); 
